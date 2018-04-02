cd9c325ff63174ad59dedc69358b4af83f0fa62f
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-04-03 01:27:56 +0900

    init

---
+++ src/server.ts
@@ -0,0 +1,26 @@
+import express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname + "/www"));
+
+app.get("/", (req, res) => {
+    console.log(req);
+    res.write("foo");
+    res.end();
+});
+
+app.get("/hello", (req, res) => {
+    res.sendFile(__dirname + "/www/index.html");
+});
+
+app.get("/search/:name", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    let name = req.params.name;
+    res.json(name);
+    res.end();
+});
+
+app.listen(8081);
+console.log("server is running.");
