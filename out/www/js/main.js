917d483885ad9bd7c6cde80d907570dd0e60f74f
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-07-18 22:54:40 +0900

    Fix parameters.

--- out/www/js/main.js
+++ out/www/js/main.js
@@ -5,12 +5,12 @@ $(document).on("click", ".program_item", function () {
     console.log(programName);
 });
 
-let total = 10;
-$("#send_lines").click(function () {
+let total = 3000;
+$(document).on("click", "#send_lines", function () {
     $("#right_bar").empty();
     let lines = $("#lines").val().split(",");
     for (let line of lines) {
         let pos = (line - 1) / total * 100;
-        $("#right_bar").append("<div class=\"rect\" style=\"top:" + pos + "%; height: " + (1 / total * 100) + "%\"></div>");
+        $("#right_bar").append("<div class=\"rect\" style=\"top:" + pos + "%; height: 10px\"></div>");
     }
 });
