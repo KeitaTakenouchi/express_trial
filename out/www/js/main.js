54f16de83985b18c5d7e9c5cdac0c631edefbf51
Takenouchi Keita <t.keita.osaka@gmail.com>
2018-07-18 22:47:00 +0900

    Add some trivial items.

--- out/www/js/main.js
+++ out/www/js/main.js
@@ -1,6 +1,16 @@
 
 
 $(document).on("click", ".program_item", function () {
-    var text = $(this).text();
-    console.log(text);
+    var programName = $(this).text();
+    console.log(programName);
+});
+
+let total = 10;
+$("#send_lines").click(function () {
+    $("#right_bar").empty();
+    let lines = $("#lines").val().split(",");
+    for (let line of lines) {
+        let pos = (line - 1) / total * 100;
+        $("#right_bar").append("<div class=\"rect\" style=\"top:" + pos + "%; height: " + (1 / total * 100) + "%\"></div>");
+    }
 });
